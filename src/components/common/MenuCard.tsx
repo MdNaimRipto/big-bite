46711a7c18aee4d7db2ecb158cb8e3698a120c29
MdNaimRipto <naimurtsc567@gmail.com>
2024-02-25 14:55:18 +0600

    Menu Card Updated

--- src/components/common/MenuCard.tsx
+++ src/components/common/MenuCard.tsx
@@ -12,17 +12,17 @@ interface IDish {
 const MenuCard = ({ dish, index }: { dish: IDish; index: number }) => {
   return (
     <div className="group">
-      <div className="h-full w-full p-10 border border-white group-hover:border-primary rounded duration-700">
-        <div className="mb-5 w-32 h-32 mx-auto">
+      <div className="h-full w-full duration-700 overflow-hidden">
+        <div className="mb-5 w-full h-[220px] overflow-hidden p-2">
           <Image
             src={dish.img}
             alt={`Dish-Image0${index}`}
-            className="group-hover:rotate-[360deg] duration-[5s]"
+            className="group-hover:scale-105 duration-[.3s] w-full h-full object-cover rounded-t-xl rounded-b-sm"
             width={400}
             height={400}
           />
         </div>
-        <div className="text-center">
+        <div className="text-center px-10 pb-10">
           <h2 className="text-2xl titleFont mb-3 text-black font-semibold">
             {dish.title}
           </h2>
@@ -35,7 +35,7 @@ const MenuCard = ({ dish, index }: { dish: IDish; index: number }) => {
             sx={{
               color: colorConfig.white,
               py: 1,
-              px: 2,
+              px: 4,
             }}
           >
             Add To Cart
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,7 +1,6 @@
 import Desserts from "@/components/menu/Desserts";
 import Drinks from "@/components/menu/Drinks";
 import MainDishes from "@/components/menu/MainDishes";
-import MostPopularDishes from "@/components/menu/MostPopularDishes";
 import StarterDishes from "@/components/menu/StarterDishes";
 import MainLayout from "@/layouts/MainLayout";
 import React, { ReactElement } from "react";
@@ -9,7 +8,6 @@ import React, { ReactElement } from "react";
 const Menu = () => {
   return (
     <div className="container px-4 my-24">
-      <MostPopularDishes />
       <StarterDishes />
       <MainDishes />
       <Drinks />
